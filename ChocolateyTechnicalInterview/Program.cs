a992ff4a2be9ee0a2fb06f44cb29c864d1c66d4a
Karijanna Miller <narae7595@ksu.edu>
2019-11-01 14:27:09 -0500

    First commit

---
+++ ChocolateyTechnicalInterview/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateyTechnicalInterview
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] array = { "madam", "step on no pets", "book"};
+            foreach (string str in array)
+            {
+                Console.WriteLine("{0} is {1}", str, chkPalindrome(str));
+            }
+        }
+
+        internal static string chkPalindrome(string str)
+        {
+            int length = str.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (str[i] != str[length - i - 1])
+                {
+                    return "Not Palindrome";
+                }
+            }
+            return "Palindrome";
+        }
+    }
+}
