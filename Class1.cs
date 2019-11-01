37750f5ea25912e2481c7e1b2630223571e0f5a5
Karijanna Miller <narae7595@ksu.edu>
2019-11-01 14:09:08 -0500

    FIrst commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine("hellu");
+        }
+    }
+}
